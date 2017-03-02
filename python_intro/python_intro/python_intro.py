6510700299a8b33fafab74cf70920e2595e2a2a5
Kamila <orzelkam11@o2.pl>
2017-03-02 22:14:19 +0100

    test message 1

---
+++ python_intro/python_intro/python_intro.py
@@ -0,0 +1,29 @@
+print('Hello, Django girls!')
+if (4 > 3) :
+    print('I was right')
+if 3 > 2 :
+    print("tak jest")
+else :
+    print('nie jest tak')    
+
+name = 'Sonja'
+if name == 'Ola':
+    print('Hej Ola!')
+elif name == 'Sonja':
+    print('Hej Sonja!')
+else:
+    print('Hej nieznajoma!')
+
+glosnosc = 57
+if glosnosc < 20:
+    print("Prawie nic nie slychac.")
+elif 20 <= glosnosc < 40:
+    print("O, muzyka leci w tle.")
+elif 40 <= glosnosc < 60:
+    print("Idealnie, moge uslyszec wszystkie detale")
+elif 60 <= glosnosc < 80:
+    print("Dobre na imprezy")
+elif 80 <= glosnosc < 100:
+    print("Troszeczke za gloano!")
+else:
+    print("Ojoj! Moje uszy! :(")
